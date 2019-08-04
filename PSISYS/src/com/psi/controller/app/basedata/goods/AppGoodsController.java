85a768d81bb915239ad8e9f8d5a77242712513d0
ZengQingAN <1364985939@qq.com>
2019-08-04 21:23:45 +0800

    修改     appGoodsControler

--- PSISYS/src/com/psi/controller/app/basedata/goods/AppGoodsController.java
+++ PSISYS/src/com/psi/controller/app/basedata/goods/AppGoodsController.java
@@ -107,7 +107,9 @@ public class AppGoodsController extends BaseController {
 				lists.get(i).put("PostionNum", PostionNum);
 			}*/
 			lists.get(i).put("stockNum", fpd);
-			pd.put("PK_SOBOOKS", fpd.get(0).getString("PK_SOBOOKS"));
+			if(fpd!=null&&fpd.size()!=0){
+				pd.put("PK_SOBOOKS", fpd.get(0).getString("PK_SOBOOKS"));
+			}
 			pd.put("GOODCODE", GOODCODE);
 			
 			List<PageData> lpd=appSalelbillService.listDataAndNumAndPrice(pd); //获取商品最近五次销售信息
@@ -137,7 +139,9 @@ public class AppGoodsController extends BaseController {
 					lists.get(i).put("PostionNum", PostionNum);
 				}*/
 				lists.get(i).put("stockNum", fpd);
-				pd.put("PK_SOBOOKS", fpd.get(0).getString("PK_SOBOOKS"));
+				if(fpd!=null&&fpd.size()!=0){
+					pd.put("PK_SOBOOKS", fpd.get(0).getString("PK_SOBOOKS"));
+				}
 				pd.put("GOODCODE", GOODCODE);
 				
 				List<PageData> lpd=appSalelbillService.listDataAndNumAndPrice(pd);//获取商品最近五次销售信息
