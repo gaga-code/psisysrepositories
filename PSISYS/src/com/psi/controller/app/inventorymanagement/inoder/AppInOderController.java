69d42efce385e270d51955ffbede33d79e882f49
ZengQingAN <1364985939@qq.com>
2019-08-20 11:07:36 +0800

    肯定行的 AppInOderCOntroller.java

--- PSISYS/src/com/psi/controller/app/inventorymanagement/inoder/AppInOderController.java
+++ PSISYS/src/com/psi/controller/app/inventorymanagement/inoder/AppInOderController.java
@@ -161,7 +161,7 @@ public class AppInOderController extends BaseController{
 				list.add(map);
 				
 			}*/
-			if(lpd!=null){
+			if(lpd!=null&&lpd.size()!=0){
 				map.put("listNum", lpd);
 				map.put("yearMouth", str);
 				list.add(map);
@@ -267,7 +267,7 @@ public class AppInOderController extends BaseController{
 				map.put("yearMouthDay", str);
 				list.add(map);
 			}*/
-			if(lpd!=null){
+			if(lpd!=null&&lpd.size()!=0){
 				map.put("listNum", lpd);
 				map.put("yearMouth", str);
 				list.add(map);
