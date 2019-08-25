ab61d1c327b00f21f034ce8c78857934b5f34280
ZengQingAN <1364985939@qq.com>
2019-08-25 17:19:53 +0800

    提交BZBillController

--- PSISYS/src/com/psi/controller/basedata/bzbill/BZBillController.java
+++ PSISYS/src/com/psi/controller/basedata/bzbill/BZBillController.java
@@ -64,7 +64,7 @@ public class BZBillController extends BaseController{
 			String NOTE,
 			String PICTURE) throws Exception{
 		logBefore(logger, Jurisdiction.getUsername()+"修改对账单");
-	//	if(!Jurisdiction.buttonJurisdiction(menuUrl, "edit")){return null;} //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "edit")){return null;} //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
@@ -72,9 +72,10 @@ public class BZBillController extends BaseController{
 		pd.put("PICTURE",PICTURE);
 		if(Jurisdiction.buttonJurisdiction(menuUrl, "edit")){
 			if (null != file && !file.isEmpty()) {
-				String filePath = PathUtil.getClasspath() + Const.FILEPATHIMG ;		//文件上传路径/uploadFiles/uploadImgs/20190823
+				String  ffile = DateUtil.getDays();
+				String filePath = PathUtil.getClasspath() + Const.FILEPATHIMG+ffile ;		//文件上传路径/uploadFiles/uploadImgs/20190823
 				fileName = FileUpload.fileUp(file, filePath, this.get32UUID());				//执行上传a89b818383414c77bfe6bfc655b4ffed.jpg
-				pd.put("PICTURE", fileName);
+				pd.put("PICTURE", ffile+"/"+fileName);
 			}else{
 				System.out.println("上传失败");
 			}
